fcaa612b98a8488bf9edf4f71fcb7ed08ee614be
Dgeyms <Dgeyms@gmail.com>
2022-12-05 23:03:57 +0300

    Создал подключение к базе банных SQLIte.

--- src/main/java/ConnectionSQLIte.java
+++ src/main/java/ConnectionSQLIte.java
@@ -1,3 +1,15 @@
-public class ConnectionSQLIte {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class ConnectionSQLIte {
+    public void connectSQLIte(){
+        try{
+                Class.forName("org.sqlite.JDBC");
+            Connection co = DriverManager.getConnection("jdbc:sqlite:C:\\SQLite\\CurrencyExchangeDatabase.db");
+            System.out.println("Connect");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ConnectionSQLIte connectionSQLIte = new ConnectionSQLIte();
+        connectionSQLIte.connectSQLIte();
+    }
+
+}
